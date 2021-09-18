0b968d9de865e8ca2889ed7a7f3c357a95b5ec32
seyi-baks <oluwaseyi.bakare@gmail.com>
2021-09-18 17:16:54 +0100

    User details validation & Hashing Password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,9 +1,14 @@
-import { IsEmail, IsString } from "class-validator";
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialsDto {
     @IsEmail()
+    @MinLength(4)
+    @MaxLength(20)
     email: string;
 
     @IsString()
+    @MinLength(8)
+    @MaxLength(32)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: "Passowrd is too weak"})
     password: string;
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,11 +1,23 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
+import * as argon2 from "argon2";
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
     async createUser(authCredentials: AuthCredentialsDto): Promise<void> {
-        const user = this.create(authCredentials)
-        await this.save(user)
+        const {email, password} = authCredentials;
+
+        try {
+            const hashedPassword = await argon2.hash(password);
+            const user = this.create({email, password: hashedPassword});
+            await this.save(user)
+        } catch (error) {
+            //Error code for duplicates in TypeORM
+            if(error.code === '23505'){
+                throw new ConflictException("Username already exists");
+            }
+        }
     }
 }
