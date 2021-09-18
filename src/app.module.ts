eecdcc0c3ddf83b573efb59b8e3b559ab9969ab1
seyi-baks <oluwaseyi.bakare@gmail.com>
2021-09-18 16:15:29 +0100

    TypeORM database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number.parseInt(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    autoLoadEntities: true
+  })],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
